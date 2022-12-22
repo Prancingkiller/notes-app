19ec8301d4d7527a82570ec1cedb705391258517
Prancingkiller <dimste18@hotmail.com>
2022-12-22 13:33:24 +0100

    app.js

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,54 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+		<nav class="navbar navbar-expand-md navbar-dark bg-dark mb-4">
+	<div class="container-fluid">
+		<router-link to="/" class="navbar-brand">Home</router-link>
+		<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+			<span class="navbar-toggler-icon"></span>
+		</button>
+		<div class="collapse navbar-collapse" id="navbarCollapse">
+			<ul class="navbar-nav me-auto mb-2 mb-md-0">
+				<li class="nav-item">
+					<router-link class="nav-link" to="/notes">Notes</router-link>
+				</li>
+				<li class="nav-item">
+					<router-link class="nav-link" to="/calendar">Calendar</router-link>
+				</li>
+			</ul>
+			<form id="authForm" class="d-flex" style="flex-wrap: wrap;flex-direction: column;">
+				<LoginForm />
+			</form>
+		</div>
+	</div>
+</nav>
+	<router-view/>
 </template>
-
-<style lang="scss">
+<script>
+	import LoginForm from "./components/LoginForm"
+	export default{
+	components:{
+		LoginForm
+	}
+}
+</script>
+<style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+	font-family: Avenir, Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #2c3e50;
 }
 
 nav {
-  padding: 30px;
+	padding: 30px;
+}
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
+nav a {
+	font-weight: bold;
+	color: #2c3e50;
+}
 
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+nav a.router-link-exact-active {
+	color: #42b983;
 }
 </style>
