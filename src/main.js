8b8e97830c0c06b42b148c6ca1f6a707c7a541ae
Prancingkiller <dimste18@hotmail.com>
2022-12-29 19:22:07 +0100

    salary DB

--- src/main.js
+++ src/main.js
@@ -5,5 +5,9 @@ import router from './router'
 import Modal from 'vue-bs-modal'
 import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap"
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
+library.add(faUserSecret)
 
-createApp(App).use(router).use(Modal).mount('#app')
+createApp(App).use(router).use(Modal).component('font-awesome-icon', FontAwesomeIcon).mount('#app')
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
+    <img alt="Notes App" src="../assets/icon-256x256.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
