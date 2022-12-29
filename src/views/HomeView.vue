cde5263dad01228ee4cf50fb28500373c36d3c7f
Prancingkiller <dimste18@hotmail.com>
2022-12-29 19:24:04 +0100

    salary DB

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,14 @@
 <template>
   <div class="home">
     <img alt="Notes App" src="../assets/icon-256x256.png">
+    <div class="row">
+      <div class="col-6">
+        <font-awesome-icon icon="fa-solid fa-notes" />
+      </div>
+      <div class="col-6">
+        <font-awesome-icon icon="fa-solid fa-calendar" />
+      </div>
+    </div>
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
