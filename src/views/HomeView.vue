878b978b0273ca44b9601a17f76049674965c66a
Prancingkiller <dimste18@hotmail.com>
2022-12-29 19:48:57 +0100

    salary DB

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="container">
-    <div class="row">
+    <div class="row" style="max-width:500px">
       <div class="col-6">
         <font-awesome-icon icon="note-sticky" class="fa-10x" @click="goNotes" />
       </div>
@@ -8,22 +8,18 @@
         <font-awesome-icon icon="calendar" class="fa-10x" @click="goCalendar" />
       </div>
     </div>
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+
 import indexedMethods from "../api/resources/indexedMethods"
 import { useRouter, useRoute } from 'vue-router'
 
 
 export default {
   name: 'HomeView',
-  components: {
-    HelloWorld
-  },
+
   setup(){
     const router = useRouter()
 		const db = indexedMethods.initiate();
