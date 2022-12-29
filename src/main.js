18aa268a2599ac61a8958fd4fe1dd3184c25ab74
Prancingkiller <dimste18@hotmail.com>
2022-12-29 19:34:11 +0100

    salary DB

--- src/main.js
+++ src/main.js
@@ -8,6 +8,6 @@ import "bootstrap"
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import { faNotes,faCalendar } from '@fortawesome/free-solid-svg-icons'
-library.add(faNotes).add(faCalendar)
+library.add(faNotes)
 
 createApp(App).component('font-awesome-icon', FontAwesomeIcon).use(router).use(Modal).mount('#app')
