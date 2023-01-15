a5c4b9f5c1df5582aa60ce30c84572accf7e7120
Prancingkiller <dimste18@hotmail.com>
2023-01-15 13:40:54 +0100

    test notes

--- src/components/BaseNote.vue
+++ src/components/BaseNote.vue
@@ -1,16 +1,17 @@
 <template>
-	<div style="max-width:150px" class="card">
-		<img src="../assets/icon-256x256.png" class="card-img-top" alt="...">
+	<div v-if="note.temp==true" style="max-width:150px;background-color: #0000002b;" class="card">
 		<div class="card-body">
 			<h5 class="card-title">{{note.title}}</h5>
 			<p class="card-text">{{note.text}}</p>
-		<button class="btn btn-danger" @touchstart="$emit('delete', note)" @click="$emit('delete', note)" >Cancella</button>
+			<button class="btn btn-danger" @touchstart="$emit('delete', note)" @click="$emit('delete', note)" >Cancella</button>
+		</div>
 	</div>
-	<div class="card-footer">
-			<p class="card-text">
-				<small v-if="note.temp==true" class="text-muted">Not Synced...</small>
-				<small v-else class="text-muted">Online</small></p>
-	</div>
+	<div v-else style="max-width:150px" class="card">
+		<div class="card-body">
+			<h5 class="card-title">{{note.title}}</h5>
+			<p class="card-text">{{note.text}}</p>
+			<button class="btn btn-danger" @touchstart="$emit('delete', note)" @click="$emit('delete', note)" >Cancella</button>
+		</div>
 	</div>
 </template>
 <script>
