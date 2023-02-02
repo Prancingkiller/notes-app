92c7aaf526d82ad5ae3e1def8c22b51c86ca1f6c
Prancingkiller <dimste18@hotmail.com>
2023-02-02 12:04:12 +0100

    login change

--- src/views/NotesView.vue
+++ src/views/NotesView.vue
@@ -45,7 +45,7 @@ export default{
 		const indexedDB = ref({}); 
 		const modalRef = ref(null);
 		var conn;
-		var timer;
+		//var timer;
 		var modal = Modal ;
 		onMounted(init)
 		onUnmounted(closeSocket)
@@ -74,13 +74,13 @@ export default{
 			},
 			function() {
 				console.warn('WebSocket connection closed');
-				timer = setTimeout(socket,5000)
+				//timer = setTimeout(socket,5000)
 			},
 			{'skipSubprotocolCheck': true}
 			);
 		}
 		function closeSocket(){
-			clearTimeout(timer)
+			//clearTimeout(timer)
 			conn.close();
 		}
 	
