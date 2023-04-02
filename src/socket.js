259c955397957d037ee5c39551c8616f80874dcb
Prancingkiller <dimste18@hotmail.com>
2023-04-02 18:14:46 +0200

    socket.io

--- src/socket.js
+++ src/socket.js
@@ -1,16 +1,15 @@
 import { reactive } from "vue";
-import { Manager  } from "socket.io-client";
+import { io } from "socket.io-client";
 
 export const state = reactive({
   connected: false,
   NotesEvents: []
 });
 
-const manager = new Manager("https://notes-api.it:8443",{
+export const socket = io("https://notes-api.it:8443",{
     reconnection: false,
     forceNew: true
     });
-export const socket = manager.socket("/");
 
 socket.on("connect", () => {
   state.connected = true;
