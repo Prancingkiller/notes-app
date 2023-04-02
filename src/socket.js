6453911f63f3ba351dcc980204e699bd88d493cd
Prancingkiller <dimste18@hotmail.com>
2023-04-02 02:11:47 +0200

    socket.io

---
+++ src/socket.js
@@ -0,0 +1,32 @@
+import { reactive } from "vue";
+import { io } from "socket.io-client";
+
+export const state = reactive({
+  connected: false,
+  fooEvents: [],
+  barEvents: []
+});
+
+// "undefined" means the URL will be computed from the `window.location` object
+const URL = process.env.NODE_ENV === "production" ? undefined : "http://localhost:3000";
+
+export const socket = io("https://notes-api.it:8443",{
+    reconnection: false
+    });
+
+socket.on("connect", () => {
+  state.connected = true;
+  socket.emit('subscribe', localStorage.getItem("unique_id"));
+});
+
+socket.on("disconnect", () => {
+  state.connected = false;
+});
+
+const tryReconnect = () => {
+    setTimeout(() => {
+      socket.connect()
+    }, 3000);
+  }
+
+socket.io.on("close", tryReconnect);
