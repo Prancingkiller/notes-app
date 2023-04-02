ca292f1fe318564f732f281a15758ec3d5b5d1ce
Prancingkiller <dimste18@hotmail.com>
2023-04-02 02:32:24 +0200

    socket.io

--- src/socket.js
+++ src/socket.js
@@ -6,9 +6,6 @@ export const state = reactive({
   NotesEvents: []
 });
 
-// "undefined" means the URL will be computed from the `window.location` object
-const URL = process.env.NODE_ENV === "production" ? undefined : "http://localhost:3000";
-
 export const socket = io("https://notes-api.it:8443",{
     reconnection: false
     });
