332ea6691af3bb740fdc46edc82a8ab4df093cb5
Prancingkiller <dimste18@hotmail.com>
2023-05-06 00:14:01 +0200

    test manager

--- src/views/ManagerView.vue
+++ src/views/ManagerView.vue
@@ -34,7 +34,7 @@
 <script>
 import ManagerMethods from "@/api/resources/ManagerMethods";
 import {ref} from "vue"
-import { VueDraggableNext } from 'vue-draggable-next'
+import nestedDraggable from "../components/NestedDraggable.vue";
 export default{
 	setup(){
 		var full = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54];
@@ -90,7 +90,7 @@ export default{
 		return{workers,slots,days,makeShift,full,minTimeBetweenShifts,tableResult,shift}
 	},
 	components:{
-		
+		nestedDraggable
 	}
 }
 </script>
