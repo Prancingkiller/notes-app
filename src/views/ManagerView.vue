761bcc8776d184213554f7ef4fe49ebc7d4b82e1
Prancingkiller <dimste18@hotmail.com>
2023-06-09 22:07:06 +0200

    test manager

--- src/views/ManagerView.vue
+++ src/views/ManagerView.vue
@@ -103,7 +103,7 @@ export default{
 		var morning = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30];
 		//var afternoon = [28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54];
 		const workers = ref([
-		{name:"Salome",label:"Salome",id:21,hours:18,SlotDays:{Lun:[],Mar:[],Mer:[],Gio:[],Ven:full,Sab:full,Dom:full},favouriteSlots:{Lun:[],Mar:[],Mer:[],Gio:[],Ven:full,Sab:full,Dom:full}},
+		{name:"Salome",label:"Salome",id:21,hours:18,SlotDays:{Lun:[],Mar:[],Mer:[],Gio:[],Ven:full,Sab:full,Dom:full},favouriteSlots:{Lun:[],Mar:[],Mer:[],Gio:[],Ven:full,Sab:full,Dom:[12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27]}},
 {name:"Stefano",label:"Stefano",id:4,hours:21,SlotDays:{Lun:full,Mar:full,Mer:full,Gio:full,Ven:full,Sab:full,Dom:full},favouriteSlots:{Lun:full,Mar:full,Mer:full,Gio:full,Ven:full,Sab:full,Dom:full}},
 {name:"Marco",label:"Marco",id:23,hours:18,SlotDays:{Lun:[],Mar:[],Mer:[],Gio:[],Ven:full,Sab:full,Dom:full},favouriteSlots:{Lun:[],Mar:[],Mer:[],Gio:[],Ven:full,Sab:full,Dom:full}},
 {name:"Luca",label:"Luca",id:24,hours:24,SlotDays:{Lun:full,Mar:full,Mer:full,Gio:full,Ven:full,Sab:full,Dom:full},favouriteSlots:{Lun:full,Mar:full,Mer:full,Gio:full,Ven:full,Sab:full,Dom:full}},
