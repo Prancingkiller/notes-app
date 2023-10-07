00353cf8c32dd5976c7bb4608ab4731d7efc9bbf
Prancingkiller <dimste18@hotmail.com>
2023-10-07 16:38:56 +0200

    calendar

--- src/views/ManagerView.vue
+++ src/views/ManagerView.vue
@@ -258,7 +258,7 @@ export default {
 		}
 		function updateSelectedDay(e: any) {
 			selectedDay.value = e.endDate.toISOString().split('T')[0];
-			// console.log(e.endDate.toISOString().split('T')[0]);
+			console.log(e.endDate.toISOString().split('T')[0]);
 		}
 		function togglePanel(index: number) {
 			workers.value[index].showDays = !workers.value[index].showDays;
