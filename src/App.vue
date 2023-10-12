007bb75e37120cbdc06dc9736fac04f18f3995a7
Prancingkiller <dimste18@hotmail.com>
2023-10-12 17:01:49 +0200

    first

--- src/App.vue
+++ src/App.vue
@@ -1,53 +1,54 @@
 <template>
-		<nav class="navbar navbar-expand-md navbar-dark bg-dark mb-4">
-	<div class="container-fluid">
-		<router-link to="/" class="navbar-brand">Home</router-link>
-		<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
-			<span class="navbar-toggler-icon"></span>
-		</button>
-		<div class="collapse navbar-collapse" id="navbarCollapse">
-			<ul class="navbar-nav me-auto mb-2 mb-md-0">
-				<li class="nav-item">
-					<router-link class="nav-link" to="/notes">Notes</router-link>
-				</li>
-				<li class="nav-item">
-					<router-link class="nav-link" to="/calendar">Calendar</router-link>
-				</li>
-			</ul>
+	<nav class="navbar navbar-expand-md navbar-dark bg-dark mb-4">
+		<div class="container-fluid">
+			<router-link to="/" class="navbar-brand">Home</router-link>
+			<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse"
+				aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+				<span class="navbar-toggler-icon"></span>
+			</button>
+			<div class="collapse navbar-collapse" id="navbarCollapse">
+				<ul class="navbar-nav me-auto mb-2 mb-md-0">
+					<li class="nav-item">
+						<router-link class="nav-link" to="/notes">Notes</router-link>
+					</li>
+					<li class="nav-item">
+						<router-link class="nav-link" to="/calendar">Calendar</router-link>
+					</li>
+				</ul>
 				<LoginForm />
+			</div>
 		</div>
-	</div>
-</nav>
-	<router-view/>
+	</nav>
+	<router-view />
 </template>
 <script>
 import { onMounted } from 'vue'
 import LoginForm from "./components/LoginForm"
-	export default{
-	components:{
+export default {
+	components: {
 		LoginForm
 	},
-	setup(){
-		function registerSyncP(){
-			if(localStorage.getItem("logged")){
-			navigator.serviceWorker.ready.then((registration) => {
-				registration.sync.getTags().then((tags) => {
-				if (tags.includes("notes_add")){
-					console.log("Sync already registered");
-				}
-				else{
-					registration.sync.register("notes_add");
-					console.log("Periodic background sync registered!");
-				}
-				setTimer();
+	setup() {
+		function registerSyncP() {
+			if (localStorage.getItem("logged")) {
+				navigator.serviceWorker.ready.then((registration) => {
+					registration.sync.getTags().then((tags) => {
+						if (tags.includes("notes_add")) {
+							console.log("Sync already registered");
+						}
+						else {
+							registration.sync.register("notes_add");
+							console.log("Periodic background sync registered!");
+						}
+						setTimer();
+					});
 				});
-			});
-			}else{setTimer();}
+			} else { setTimer(); }
 		}
-		function setTimer(){
-		setTimeout(registerSyncP, 10000);
+		function setTimer() {
+			setTimeout(registerSyncP, 10000);
 		}
-		function handlerSync(){
+		function handlerSync() {
 			if ('serviceWorker' in navigator && 'SyncManager' in window) {
 				setTimer()
 			} else {
