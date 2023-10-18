6b7c414a2061b3abfec9e2b7dd5784ffac4053c2
Prancingkiller <dimste18@hotmail.com>
2023-10-18 20:24:35 +0200

    manager

--- src/components/BaseOptions.vue
+++ src/components/BaseOptions.vue
@@ -3,13 +3,14 @@
     <div>
         <div v-for="(option,i) in userGroup.options" :key="i">
             {{ option.name }}
-            <BaseInput v-if="option.type=='number' || option.type=='date'" :type="option.type" v-model="option.value"></BaseInput>
-            
+            <BaseInput v-if="option.type=='number' || option.type=='time'" :type="option.type" v-model="option.value"></BaseInput>
+            <BaseCheckbox v-if="option.type=='boolean'" v-model="option.value"></BaseCheckbox>
         </div>
     </div>
 </template>
 <script>
 import BaseInput from './BaseInput.vue'
+import BaseCheckbox from './BaseCheckbox.vue'
 export default {
     props: {
         userGroup: {
@@ -17,7 +18,7 @@ export default {
         }
     },
     components:{
-        BaseInput
+        BaseInput,BaseCheckbox
     }
 }
 </script>
