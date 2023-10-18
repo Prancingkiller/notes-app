7ac5b568fbd2434549edc9b5111b33cde0530da0
Prancingkiller <dimste18@hotmail.com>
2023-10-18 20:21:18 +0200

    manager

--- src/views/directorsRoom.vue
+++ src/views/directorsRoom.vue
@@ -6,41 +6,41 @@
         <button @click="setContent('resourcesOptions')">Resources Manager</button>
     </div>
     <div>
-        <div v-if="content=='generalOptions'">
-            <BaseOptions  v-for="(set,i) in options" :key="i" :userGroup="set"></BaseOptions>
+        <div v-if="content == 'generalOptions'">
+            <BaseOptions v-for="(set, i) in options" :key="i" :userGroup="set"></BaseOptions>
         </div>
     </div>
 </template>
 <script>
-import {ref,onMounted} from 'vue'
+import { ref, onMounted } from 'vue'
 import { useRouter } from 'vue-router'
 import BaseOptions from '../components/BaseOptions.vue'
 import directorMethods from '../api/resources/directorMethods.js'
-export default{
-    components:{
-        BaseOptions
-    },
-    setup(){
+export default {
+    setup() {
         const router = useRouter();
         const content = ref(null);
         const options = ref([]);
 
-        async function getOptions(){
-            options.value = (directorMethods.getAllOptions());
+        async function getOptions() {
+            options.value = await directorMethods.getAllOptions();
         }
 
-        onMounted(()=>{
+        onMounted(() => {
             getOptions();
         })
 
-        function setContent(data){
+        function setContent(data) {
             content.value = data;
             console.log(options.value);
         }
 
-        return{
-            content,router,options,setContent
+        return {
+            content, router, options, setContent
         }
-    }
+    },
+    components: {
+        BaseOptions
+    },
 }
 </script>
