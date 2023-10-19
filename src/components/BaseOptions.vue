207cab054930c0b97f530f2946875cb8b228e4c0
Prancingkiller <dimste18@hotmail.com>
2023-10-19 19:03:52 +0200

    manager

--- src/components/BaseOptions.vue
+++ src/components/BaseOptions.vue
@@ -1,6 +1,6 @@
 <template>
         <div>
-            <h3>Opzioni per gruppo: {{ userGroup.user_group }}</h3>
+            <h3>Opzioni per gruppo: {{ data.user_group }}</h3>
             <table class="table table-sm">
                 <thead>
                     <tr>
@@ -12,35 +12,48 @@
                 <tbody>
                     <tr>
                         <td>Apertura</td>
-                        <td><input type="time" v-model="userGroup.options.apertura"></td>
+                        <td><input type="time" v-model="data.options.apertura"></td>
                     </tr>
                     <tr>
                         <td>Chiusura</td>
-                        <td><input type="time" v-model="userGroup.options.chiusura"></td>
+                        <td><input type="time" v-model="data.options.chiusura"></td>
                     </tr>
                     <tr>
                         <td>Pausa minima tra turni (h)</td>
-                        <td><input type="number" v-model="userGroup.options.minTimeBetweenShifts"></td>
+                        <td><input type="number" v-model="data.options.minTimeBetweenShifts"></td>
                     </tr>
                     <tr>
                         <td>Turno minimo (h)</td>
-                        <td><input type="number" v-model="userGroup.options.baseShift"></td>
+                        <td><input type="number" v-model="data.options.baseShift"></td>
                     </tr>
                     <tr>
                         <td>Abilita doppi turni</td>
-                        <td><input type="checkbox" v-model="userGroup.options.allowDoubleShifts"></td>
+                        <td><input type="checkbox" v-model="data.options.allowDoubleShifts"></td>
                     </tr>
                 </tbody>
             </table>
         </div>
 </template>
 <script>
-
 export default {
     props: {
         userGroup: {
-            type: Object
+            type: {
+                user_group:Number,
+                options:{
+                    allowDoubleShifts:Boolean,
+                    baseShift:Number,
+                    minTimeBetweenShifts:Number,
+                    chiusura:String,
+                    apertura:String
+                }
+            }
         }
     },
+    setup(props){
+        const data = props.userGroup;
+
+        return{data}
+    }
 }
 </script>
