9a3a77636d0cf05aa8a6d69fe8d7cc5c665bd479
Prancingkiller <dimste18@hotmail.com>
2023-10-19 19:28:18 +0200

    manager

--- src/api/resources/directorMethods.js
+++ src/api/resources/directorMethods.js
@@ -9,15 +9,15 @@ export default {
             method: 'GET',
         });
         const result = await response.json();
-        let returns = {
-            apertura: result.apertura,
-            chiusura: result.chiusura,
-            minTimeBetweenShifts: result.minTimeBetweenShifts,
-            allowDoubleShifts: result.allowDoubleShifts,
-            baseShift: result.baseShift,
-            slots: JSON.parse(result.slots)
-        }
-        console.log(returns);
-        return returns;
+        // let returns = {
+        //     apertura: result.apertura,
+        //     chiusura: result.chiusura,
+        //     minTimeBetweenShifts: result.minTimeBetweenShifts,
+        //     allowDoubleShifts: result.allowDoubleShifts,
+        //     baseShift: result.baseShift,
+        //     slots: JSON.parse(result.slots)
+        // }
+        // console.log(returns);
+        return result;
     }
 }
