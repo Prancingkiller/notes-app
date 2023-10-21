1e0087f34393e9857b4fa3ef83f3c1d80d03ef73
Prancingkiller <dimste18@hotmail.com>
2023-10-21 02:22:45 +0200

    manager

--- src/views/ManagerView.vue
+++ src/views/ManagerView.vue
@@ -127,6 +127,16 @@
 				<td><input type="number" v-model="configuration.slots.Dom[i].required" style="width:40px;border:0px"></td>
 			</tr>
 		</table>
+		<table v-for="(day,i) in days" :key="i">
+			<tr>
+				<td v-for="(slot,i) in configuration.slots[day]" :key="i">{{ slot.slotN }}</td>
+			</tr>
+			<tr>
+				<td v-for="(slot,i) in configuration.slots[day]" :key="i">
+					<input type="number" v-model="slot.required" style="width:40px;border:0px">
+				</td>
+			</tr>
+		</table>
 	</div>
 </template>
 <script lang="ts">
