39bcc9fd33b2fb4812ffa32b707d4fabc14bc008
Prancingkiller <dimste18@hotmail.com>
2023-10-21 15:04:44 +0200

    manager

--- src/components/BaseOptions.vue
+++ src/components/BaseOptions.vue
@@ -1,4 +1,5 @@
 <template>
+    <div style="display:flex;flex-direction: column;">
         <div class="p-4">
             <h3>Opzioni per gruppo: {{ data.user_group }}</h3>
             <table class="table table-sm">
@@ -26,45 +27,48 @@
             </table>
         </div>
         <div style="display:flex;flex-direction: row;">
-            <div v-for="(day,i) in days" :key="i" style="display:flex;flex-direction: column;">
+            <div v-for="(day, i) in days" :key="i" style="display:flex;flex-direction: column;">
                 {{ day }}<br>
-                <input type="time" :value="data.openings[day].apertura">
-                <input type="time" :value="data.openings[day].chiusura">
+                <input type="time" v-model="data.openings[day].apertura">
+                <input type="time" v-model="data.openings[day].chiusura">
             </div>
         </div>
+    </div>
+    <p>{{ data }}</p>
 </template>
 <script>
+import { ref } from 'vue'
 export default {
     props: {
         userGroup: {
             type: {
-                user_group:Number,
-                options:{
-                    allowDoubleShifts:Boolean,
-                    baseShift:Number,
-                    minTimeBetweenShifts:Number,
-                    chiusura:String,
-                    apertura:String,
-                    slots:Object,
-                    openings:Object
+                user_group: Number,
+                options: {
+                    allowDoubleShifts: Boolean,
+                    baseShift: Number,
+                    minTimeBetweenShifts: Number,
+                    chiusura: String,
+                    apertura: String,
+                    slots: Object,
+                    openings: Object
                 }
             }
         }
     },
-    setup(props){
-        const data = props.userGroup;
-        const days = ["Lun","Mar","Mer","Gio","Ven","Sab","Dom"];
-        if(data.user_group == 0){
+    setup(props) {
+        const data = ref(props.userGroup);
+        const days = ["Lun", "Mar", "Mer", "Gio", "Ven", "Sab", "Dom"];
+        if (data.user_group == 0) {
             data.user_group = "Crew"
         }
-        if(data.user_group == 1){
+        if (data.user_group == 1) {
             data.user_group = "Hostess"
         }
-        if(data.user_group == 2){
+        if (data.user_group == 2) {
             data.user_group = "Manager"
         }
 
-        return{data,days}
+        return { data, days }
     }
 }
 </script>
