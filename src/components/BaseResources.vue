5c4a7792dc793592fc308fdab8e51a7d5668f37d
Prancingkiller <dimste18@hotmail.com>
2023-11-03 21:05:40 +0100

    manager

--- src/components/BaseResources.vue
+++ src/components/BaseResources.vue
@@ -145,7 +145,7 @@ export default {
                 user_group: data.value.user_group,
                 hours: 0,
                 SlotDays: {
-                    Lun: [{start: "08:00",finish: "08:00",}
+                    Lun: [{start: data.rules.openings.Lun.apertura,finish: data.rules.openings.Lun.chiusura,}
                     ],
                     Mar: [{start: "08:00",finish: "08:00",}
                     ],
