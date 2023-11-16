db0b6a8af3b77aee1b5526f556489cea93faffa3
Prancingkiller <dimste18@hotmail.com>
2023-11-16 22:56:08 +0100

    manager

--- src/api/resources/ManagerMethods.ts
+++ src/api/resources/ManagerMethods.ts
@@ -64,6 +64,7 @@ export default {
     async makeShiftV3<TResponse>(data: toSend): Promise<TResponse> {
         const response = await fetch(APISettings.baseURL + "/shiftsV3.php", {
             method: "POST",
+            timeout: 60,
             mode: "cors",
             cache: "no-cache",
             headers: {
