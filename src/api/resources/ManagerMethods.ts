dda8343d67ccf4a778d200b7bf06e8c984a41f1a
Prancingkiller <dimste18@hotmail.com>
2023-11-26 13:53:23 +0100

    manager

--- src/api/resources/ManagerMethods.ts
+++ src/api/resources/ManagerMethods.ts
@@ -70,7 +70,8 @@ export default {
                 //'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8',
                 'Trusted': 'yes'
             },
-            body: data
+            body: data,
+            keepalive: true
         })
         const result = await response.json();
         if (result.data.status == "allGood") {
