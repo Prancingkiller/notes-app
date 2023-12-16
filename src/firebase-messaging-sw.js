f8bd9a3143f301b2d5e2747ef5a10212cba67467
Prancingkiller <dimste18@hotmail.com>
2023-12-16 20:29:12 +0100

    manager

--- src/firebase-messaging-sw.js
+++ src/firebase-messaging-sw.js
@@ -270,7 +270,10 @@ const messaging = getMessaging(app);
 onBackgroundMessage(messaging, (payload) => {
   console.log('[firebase-messaging-sw.js] Received background message ', payload);
   // Customize notification here
-  const notificationTitle = 'Background Message Title';
+  let notificationTitle = "Title";
+  if (payload.notification?.title) {
+    notificationTitle = payload.notification?.title
+  }
   const notificationOptions = {
     body: payload.notification?.body,
     icon: '/firebase-logo.png'
